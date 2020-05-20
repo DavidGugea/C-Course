4922ea7676a72d4552b441e19723b6686106fca0
David <davidgugea@yahoo.com>
2020-05-21 00:39:05 +0200

    Arrays

---
+++ freeCodeCamp_org/Arrays/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    int luckyNumbers[10];
+    luckyNumbers[1] = 80;
+    luckyNumbers[0] = 90;
+    */
+    char pharse[] = "Giraffe Academy";
+    printf("%d", luckyNumbers[0]);
+    return 0;
+}
