a427de817b6f97f874fe53de6f9baf0d29353d53
David <davidgugea@yahoo.com>
2020-05-20 23:14:14 +0200

    Constants

---
+++ freeCodeCamp_org/Constants/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*
+    const int FAV_NUM = 5;
+    printf("%d\n", FAV_NUM);
+    FAV_NUM = 8;
+    printf("%d", FAV_NUM);
+    */
+
+    printf("Hello"); // "Hello is also a constant
+    pritnf("%d", 90); // 90 is also considered a constant
+
+    return 0;
+}
